fd5a956911cc2903fa58b9f188c9964a4d3531f9
tommymav13-pixel <tommy.mav13@gmail.com>
2026-08-12 23:38:56 +0200

    Add chapter 1 exercise and ignore class files

--- head-first-java/chapter-01/exercises/Shuffle1.java
+++ head-first-java/chapter-01/exercises/Shuffle1.java
@@ -1,7 +1,26 @@
 // Chapter 1 exercise - Code Magnets / Shuffle1
 // Reconstruct the program yourself before checking the answer.
-class Shufflel {
-    public static void main(String [] args) {
-        
+class Shuffle1 {
+    public static void main(String[] args) {
+        int x = 3;
+
+        while (x > 0) {
+
+            if (x > 2) {
+                System.out.print("a");
+            }
+
+            x = x - 1;
+            System.out.print("-");
+
+            if (x == 2) {
+                System.out.print("b c");
+            }
+
+            if (x == 1) {
+                System.out.print("d");
+                x = x - 1;
+            }
+        }
     }
 }
